fcb76d41498595c805f584f1c30bea5ca70ef642
Alinde Öst <ost.alinde@gmail.com>
2024-11-27 16:03:33 +0100

    added all pages and also layout-files

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,57 @@
+import { Button } from "antd";
+import axios from "axios";
+import { useState } from "react";
+
+export const Home = () => {
+  const [petFirstAid, setPetFirstAid] = useState<any>([]);
+  const [places, setPlaces] = useState<any>([]);
+
+  const fetchPetFirstAid = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/pet-first-aid"
+      );
+      setPetFirstAid(response.data);
+    } catch (error) {
+      console.error("Error fetching Pet First Aid data:", error);
+    }
+  };
+
+  const fetchPlaces = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/places", {
+        params: { query: "Veterinärklinik" },
+      });
+      setPlaces(response.data.results);
+    } catch (error) {
+      console.error("Error fetching Places data:", error);
+    }
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={fetchPetFirstAid}>
+        Hämta Pet First Aid
+      </Button>
+      <ul>
+        {petFirstAid.map((doc: any) => (
+          <li key={doc.id}>
+            {doc.query} ({doc.response})
+          </li>
+        ))}
+      </ul>
+
+      <Button type="primary" onClick={fetchPlaces}>
+        Hämta Places
+      </Button>
+      <div>
+        <h1>Places</h1>
+        <ul>
+          {places.map((place: any) => (
+            <li key={place.place_id}>{place.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
