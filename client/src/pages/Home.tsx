ada1e9947f4b6859863f23df33b7feda952a492b
Alinde Öst <ost.alinde@gmail.com>
2024-12-03 14:22:51 +0100

    removed unnecessary code in home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,4 @@
-import { Button } from "antd";
-import axios from "axios";
-import { useState } from "react";
+
 import { RegisterModal } from "../components/RegisterModal";
 import {LoginModal} from "../components/LoginModal";
 import { LogoutButton } from "../components/LogoutButton";
@@ -8,31 +6,7 @@ import { useAuth } from "../contexts/AuthContext";
 
 
 export const Home = () => {
-  const [petFirstAid, setPetFirstAid] = useState<any>([]);
-  const [places, setPlaces] = useState<any>([]);
   const {user} = useAuth();
-
-  const fetchPetFirstAid = async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:3000/api/pet-first-aid"
-      );
-      setPetFirstAid(response.data);
-    } catch (error) {
-      console.error("Error fetching Pet First Aid data:", error);
-    }
-  };
-
-  const fetchPlaces = async () => {
-    try {
-      const response = await axios.get("http://localhost:3000/api/places", {
-        params: { query: "Veterinärklinik" },
-      });
-      setPlaces(response.data.results);
-    } catch (error) {
-      console.error("Error fetching Places data:", error);
-    }
-  };
 
   return (
     <>
@@ -48,28 +22,6 @@ export const Home = () => {
         <LogoutButton />
       <h3>Inloggad som: {user.name}</h3>
       </>)}
-      {/* <Button type="primary" onClick={fetchPetFirstAid}>
-        Hämta Pet First Aid
-      </Button>
-      <ul>
-        {petFirstAid.map((doc: any) => (
-          <li key={doc.id}>
-            {doc.query} ({doc.response})
-          </li>
-        ))}
-      </ul>
-
-      <Button type="primary" onClick={fetchPlaces}>
-        Hämta Places
-      </Button>
-      <div>
-        <h1>Places</h1>
-        <ul>
-          {places.map((place: any) => (
-            <li key={place.place_id}>{place.name}</li>
-          ))}
-        </ul>
-      </div> */}
     </>
   );
 };
