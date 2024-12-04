e075bd500353ba80687f1a34c57bd82a461dcf8c
Alinde Öst <ost.alinde@gmail.com>
2024-12-04 14:58:48 +0100

    added clinicsContext to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import { router } from './Router.tsx';
 import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import { ClinicsProvider } from './contexts/ClinicsContext.tsx';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthProvider>
+  <ClinicsProvider>
     <React.StrictMode>
       <RouterProvider router={router}></RouterProvider>
     </React.StrictMode>
+    </ClinicsProvider>
   </AuthProvider>
 );
