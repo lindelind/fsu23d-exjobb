91a1043d1624647b45dfa1dd691beb02ffd2ace9
Alinde Öst <ost.alinde@gmail.com>
2024-12-09 13:22:46 +0100

    fixed error with deprecated children for ant design, used items for menu

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,61 +1,67 @@
-import { Menu } from "antd";
+import {Menu } from "antd";
 import { Link, useLocation } from "react-router-dom";
 import { ChangeLanguage } from "./ChangeLanguage";
-import "../css/layout.css"
+import "../css/layout.css";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useAuth } from "../contexts/AuthContext";
-
 
 export const Navigation = () => {
   const location = useLocation();
   const [selectedKey, setSelectedKey] = useState(location.pathname);
   const { t } = useTranslation();
-   const { logout, user } = useAuth();
+  const { logout, user } = useAuth();
 
   useEffect(() => {
     setSelectedKey(location.pathname);
   }, [location]);
 
+  const items = [
+    {
+      key: "/",
+      label: <Link to="/">{t("homepage")}</Link>,
+    },
+    {
+      key: "/find-vet-clinic",
+      label: <Link to="/find-vet-clinic">{t("findvet")}</Link>,
+    },
+    user && {
+      key: "/profile",
+      label: (
+        <Link to="/profile" style={{color: "inherit"}} className="menu-item">
+          {t("profile")}
+        </Link>
+      ),
+      children: [
+        {
+          key: "/saved-clinics",
+          label: <Link to="/saved-clinics">{t("saved_clinics_nav")}</Link>,
+        },
+        {
+          key: "/logout",
+          label: <span onClick={logout}>{t("logout")}</span>,
+        },
+      ],
+    },
+    {
+      key: "/pet-first-aid",
+      label: <Link to="/pet-first-aid">{t("petfirstaid")}</Link>,
+    },
+    {
+      key: "/change-language",
+      label: <ChangeLanguage />,
+    },
+  ].filter(Boolean);
+
   return (
-    <Menu
-      role="menu"
-      theme="dark"
-      mode="horizontal"
-      selectedKeys={[selectedKey]}
-      className="app-menu"
-    >
-      <Menu.Item key="/">
-        <Link to="/">{t("homepage")}</Link>
-      </Menu.Item>
-
-      <Menu.Item key="/find-vet-clinic">
-        <Link to="/find-vet-clinic">{t("findvet")}</Link>
-      </Menu.Item>
-
-      {user && (
-        <>
-          <Menu.SubMenu
-            key="/profile"
-            title={<Link to="/profile">{t("profile")}</Link>}
-          >
-            <Menu.Item key="/saved-clinics">
-              <Link to="/saved-clinics">{t("saved_clinics_nav")}</Link>
-            </Menu.Item>
-            <Menu.Item key="/logout" onClick={logout}>
-              {t("logout")}
-            </Menu.Item>
-          </Menu.SubMenu>
-        </>
-      )}
-
-      <Menu.Item key="/pet-first-aid">
-        <Link to="/pet-first-aid">{t("petfirstaid")}</Link>
-      </Menu.Item>
-
-      <Menu.Item key="/change-language">
-        <ChangeLanguage />
-      </Menu.Item>
-    </Menu>
+      <Menu
+        role="menu"
+        theme="dark"
+        mode="horizontal"
+        selectedKeys={[selectedKey]}
+        className="app-menu"
+        items={items}
+      />
+  
   );
 };
