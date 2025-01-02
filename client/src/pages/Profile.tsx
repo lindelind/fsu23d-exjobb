0737a963f2e8e891eb7f7155f362ddc23f70b145
Alinde Öst <ost.alinde@gmail.com>
2025-01-02 23:24:07 +0100

    last minute fixes

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,30 +1,47 @@
 import { useAuth } from "../contexts/AuthContext";
 import LoginModal from "../components/LoginModal";
-import { Flex, Spin } from "antd";
+import { Button, Card, Spin } from "antd";
 import { useTranslation } from "react-i18next";
 
 export const Profile = () => {
   const { user, loading } = useAuth();
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   if (loading) {
-    return <p><Flex>
-      <Spin size="large"/>
-        </Flex></p>; 
+    return (
+      <p>
+        <Spin size="large" />
+      </p>
+    );
   }
 
   return (
     <>
-      {!user && (
+      {!user ? (
         <>
-          <LoginModal />
+          <p>{t("not_logged_in")}</p>
+          <Button type="primary"><LoginModal /></Button>
         </>
-      )}
-      {user && (
-        <>
-        <h3>{t("my_profile")}</h3>
-          <p>{user.name}</p>
-        </>
+      ) : (
+        <Card
+          title={t("my_profile")}
+          bordered={false}
+          style={{
+            width: 300,
+            margin: "0 auto",
+            borderRadius: "8px",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <p>
+            <strong>{t("name")}: </strong>
+            {user.name}
+          </p>
+          <p>
+            <strong>{t("email")}: </strong>
+            {user.email}
+          </p>
+        </Card>
       )}
     </>
   );
