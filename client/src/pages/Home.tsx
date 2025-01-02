f09e3745ae33152fa95104e6d038fdcf9b3ea45f
Alinde Öst <ost.alinde@gmail.com>
2025-01-02 19:16:45 +0100

    fixed homepage a little

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,29 @@
-
 import { SearchVet } from "../components/SearchVet";
-import "../css/layout.css"
-
-
-
+import "../css/layout.css";
+import { Content } from "antd/es/layout/layout";
 
 export const Home = () => {
+  const contentStyle: React.CSSProperties = {
+    height: "100%",
+    position: "relative",
+  };
 
   return (
     <>
-      <SearchVet />
-      
+      <Content
+        style={{
+          margin: 0,
+          minHeight: 280,
+        }}
+      >
+        <div style={contentStyle}>
+          <img className="banner-image" src="/1.png" alt="Banner" />
+          <div className="content-overlay">
+            <h1 className="banner-text">Welcome to Vet Finder</h1>
+            <SearchVet />
+          </div>
+        </div>
+      </Content>
     </>
   );
 };
