a3105fd26c5a8efb17adcc81a5aa8533565b3b93
Alinde Öst <ost.alinde@gmail.com>
2025-02-11 20:09:47 +0100

    reintroduced getVetClinicsBycity

--- server/src/firebase/Clinics/clinics.router.ts
+++ server/src/firebase/Clinics/clinics.router.ts
@@ -1,11 +1,11 @@
 
 import express from "express";
 
-import { getVetClinicsById, getVetClinicsByLocation, getVetClinicsByWildSearch } from "../Clinics/clinics.controller";
+import { getVetClinicsByCity, getVetClinicsById, getVetClinicsByLocation } from "../Clinics/clinics.controller";
 
 const router = express.Router();
-// router.get("/vet-clinics", getVetClinicsByCity);
-router.get("/vet-clinics", getVetClinicsByWildSearch);
+router.get("/vet-clinics", getVetClinicsByCity);
+// router.get("/vet-clinics", getVetClinicsByWildSearch);
 router.get("/vet-clinics/:id", getVetClinicsById);
 router.get("/vet-clinics-location", getVetClinicsByLocation);
 
